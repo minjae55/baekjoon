c715dcecaba385969408bf1a1ffbaaab41f66060
minjae <85977832+minjae55@users.noreply.github.com>
2026-02-25 16:58:35 +0900

    Delete src/ref_pratice directory

--- src/ref_pratice/Method4_Pratice.java
+++
@@ -1,9 +0,0 @@
-package ref_pratice;
-
-public class Method4_Pratice {
-
-    public static void main(String[] args) {
-
-    }
-
-}
--- src/ref_pratice/Student.java
+++
@@ -1,7 +0,0 @@
-package ref_pratice;
-
-public class Student {
-    String name;
-    int age;
-    int grade;
-}
