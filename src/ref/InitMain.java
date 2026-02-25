1660514edadf898ce58d8b91b2d5919d114e0b08
minjae <85977832+minjae55@users.noreply.github.com>
2026-02-25 16:58:24 +0900

    Delete src/ref directory

--- src/ref/InitMain.java
+++
@@ -1,9 +0,0 @@
-package ref;
-
-public class InitMain {
-    public static void main(String[] args) {
-        InitData data = new InitData();
-        System.out.println(data.value1);
-        System.out.println(data.value2);
-    }
-}
