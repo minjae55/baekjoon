302e38858b744142d59647c1756c2d88f7a17a1e
minjae <85977832+minjae55@users.noreply.github.com>
2026-02-25 16:57:49 +0900

    Delete src/lang directory

--- src/lang/clazz/math/test/LottoGeneratorMain.java
+++
@@ -1,9 +0,0 @@
-package lang.clazz.math.test;
-
-public class LottoGeneratorMain {
-
-    public static void main(String[] args) {
-        LottoGenerator lottoGenerator = new LottoGenerator();
-        lottoGenerator.getLottoNumbers();
-    }
-}
