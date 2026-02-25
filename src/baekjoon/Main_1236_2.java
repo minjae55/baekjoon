74239c96a988916a66c7c6b26ebc273e5629a99e
min jae Cho <alswodi5@gmail.com>
2026-02-25 16:28:41 +0900

    feat: test commit - github test - 260225 test

--- src/baekjoon/Main_1236_2.java
+++ src/baekjoon/Main_1236_2.java
@@ -1,22 +1,29 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main_1236_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int width = scanner.nextInt();
-        int height = scanner.nextInt();
+        int rows = scanner.nextInt();
+        int columns = scanner.nextInt();
         scanner.nextLine();
+        String[] temporaryGuardsPosition = new String[rows];
+        Character[][] guardsPosition = new Character[rows][columns];
 
-        String[] guardsPosition = new String[height];
-
-        char x = 'X';
-        int rowNeedGuards = 0;
-        int colNeedGuards = 0;
-        for (int i = 0; i < width; i++) {
-            guardsPosition[i] = scanner.nextLine();
-
+        for (int i = 0; i < rows; i++) {
+            temporaryGuardsPosition[i] = scanner.nextLine();
+            for (int j = 0; j < columns; j++) {
+                guardsPosition[i][j] = temporaryGuardsPosition[i].charAt(j);
+            }
         }
+
+        for (int i = 0; i < rows; i++) {
+        }
+
+
+
+
     }
 }
