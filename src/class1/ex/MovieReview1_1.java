d6af1cc9b8b88220094d2923b0c06518ce95184b
minjae <85977832+minjae55@users.noreply.github.com>
2026-02-25 16:56:22 +0900

    Delete src/class1 directory

--- src/class1/ex/MovieReview1_1.java
+++
@@ -1,6 +0,0 @@
-package class1.ex;
-
-public class MovieReview1_1 {
-    String title;
-    String review;
-}
--- src/class1/ex/MovieReviewMain1_1.java
+++
@@ -1,8 +0,0 @@
-package class1.ex;
-
-public class MovieReviewMain1_1 {
-
-    public static void main(String[] args) {
-
-    }
-}
