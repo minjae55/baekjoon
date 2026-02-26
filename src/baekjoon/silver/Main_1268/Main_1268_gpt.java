bc472098486c5997f066c4fa260504f8c823ca2d
minjae <85977832+minjae55@users.noreply.github.com>
2026-02-26 09:29:55 +0900

    Update Main_1268_gpt.java

--- src/baekjoon/silver/Main_1268/Main_1268_gpt.java
+++ src/baekjoon/silver/Main_1268/Main_1268_gpt.java
@@ -1,43 +1,42 @@
-package baekjoon.silver.Main_1268;
+package baekjoon.silver.main_1268;
 
 import java.util.Scanner;
-public class Main_1268_gpt {
+
+public class Main_1268_Gpt {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        int[][] arr = new int[number][5]; //고정 배열 사용
 
-        int[][] arr = new int[N][5];
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < number; i++) {
             for (int j = 0; j < 5; j++) {
-                arr[i][j] = sc.nextInt();
+                arr[i][j] = scanner.nextInt(); //int로 배열 넣기
             }
         }
 
         int max = 0;
         int answer = 0;
 
-        for (int i = 0; i < N; i++) {
-
-            int cnt = 0;
-
-            for (int j = 0; j < N; j++) {
-
-                if (i == j) continue;
+        for (int i = 0; i < number; i++) { //학생 A
+            int count = 0;
+            for (int j = 0; j < number; j++) { //학생 B
+                if ( i == j) continue; //같은 학생일 경우 무시
 
                 for (int k = 0; k < 5; k++) {
-                    if (arr[i][k] == arr[j][k]) {
-                        cnt++;
+                    if(arr[i][k] == arr[j][k]) {
+                        //A학생과 B학생의 모든 학년을 비교, 한 번이라도 같으면 count++, 비교 멈춤
+                        count++;
                         break;
                     }
                 }
             }
-
-            if (cnt > max) {
-                max = cnt;
-                answer = i;
+            if (count > max) {
+                max = count;    //맥스 값 갱신
+                answer = i;     //맥스가 바뀌면 학생 번호 바꿈
+                                //맥스랑 동일한 값이 여러개이면 번호 작은 사람 출력
+                                //비교 자체를 앞에서부터 하고 있으니 따른 예외 처리 필요 없음
             }
         }
 
