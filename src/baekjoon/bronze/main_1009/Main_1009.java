507a3e590402fdac7db196f9e6a7c002c5f68596
min jae Cho <alswodi5@gmail.com>
2026-03-01 23:06:53 +0900

    Main_1009 Commit

---
+++ src/baekjoon/bronze/main_1009/Main_1009.java
@@ -0,0 +1,31 @@
+package baekjoon.bronze.main_1009;
+
+import java.util.Scanner;
+
+public class Main_1009{
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int counter = scanner.nextInt();
+
+        for (int i = 1; i <= counter; i++) {
+            int integerA = scanner.nextInt();
+            int integerB = scanner.nextInt() % 4;
+
+            int lastDigit = 0;
+
+            if(integerB != 0) {
+                lastDigit = (int) Math.pow(integerA, integerB) % 10;
+            } else {
+                lastDigit = (int) Math.pow(integerA, 4) % 10;
+            }
+
+            if (lastDigit == 0) {
+                System.out.println(10);
+            } else {
+                System.out.println(lastDigit);
+            }
+        }
+    }
+}
