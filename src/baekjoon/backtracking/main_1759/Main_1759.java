54b16678d32e610c8adcd376a495ab747ebd6cb9
minjae <85977832+minjae55@users.noreply.github.com>
2026-03-09 22:50:49 +0900

    Add files via upload

---
+++ src/baekjoon/backtracking/main_1759/Main_1759.java
@@ -0,0 +1,58 @@
+package baekjoon.backtracking.main_1759;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main_1759 {
+
+    private static int l = 0;
+    private static int c = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //서로 다른 L개의 알파벳 소문자들로 구성
+        // 최소 한 개의 모음과 최소 두 개의 자음
+
+        l = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        List<String> arr = new ArrayList<>();
+        for (int i = 0; i < c; i++) {
+            arr.add(st.nextToken());
+        }
+
+        Collections.sort(arr);
+        recursion(arr, "");
+    }
+
+    private static void recursion(List<String> arr, String s) {
+        if(s.length() == l) {
+            System.out.println("종료 조건문 s = " + s);
+            s = "";
+            return;
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            if(s.contains(arr.get(i))) continue;
+            String removed = arr.get(i);
+            s += removed;
+            System.out.println("removed = " + removed);
+            System.out.println("s = " + s);
+
+            arr.remove(i);
+            System.out.println("지운 뒤 arr = " + arr);
+            // t w
+            recursion(arr, s);
+            arr.add(removed); // a c i s
+            System.out.println("추가 후 arr = " + arr);
+        }
+
+    }
+}
